aaefdc1cb27eb3a51d13e474640d09752af42eeb
joaolotjr <joaolotjr@gmail.com>
2022-08-30 17:28:47 -0300

    V1.05 - Edit JS

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,10 +1,42 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+function checkForMath() {
+    if(firstCard.dataset.card ===secondCard.dataser.card){
+        disableCards();
+        return;
+    }
+    unflipCard();
+
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+}
+
+function unflipCard(){
+    
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 })
+
+
 
