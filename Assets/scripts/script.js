046f08ea20691890c20b003538006d37bf90acd7
joaolotjr <joaolotjr@gmail.com>
2022-08-30 17:41:21 -0300

    V1.07 - Edit JS

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -5,6 +5,7 @@ let lockBoard = false;
 
 function flipCard() {
     if(lockBoard) return;
+    if(this === firstCard) return;
     this.classList.add('flip');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -18,7 +19,7 @@ function flipCard() {
 }
 
 function checkForMath() {
-    if(firstCard.dataset.card ===secondCard.dataser.card){
+    if(firstCard.dataset.card === secondCard.dataset.card){
         disableCards();
         return;
     }
@@ -30,19 +31,31 @@ function disableCards(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
 
+    resetBoard();
+
 }
 
 function unflipCard(){
+    lockBoard = true;
+
     setTimeout(()=>{
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
+
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
-})
+});
 
 
 
